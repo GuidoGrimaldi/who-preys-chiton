b715e7a2cf3ee7ae75c74c3a92793ad6cdeae543
GuidoGrimaldi <guidobio@ufrn.edu.br>
2025-06-13 10:24:14 -0300

    Finished

--- code-script.R
+++ code-script.R
@@ -10,25 +10,23 @@ set_flextable_defaults(
   font.family = "Inconsolata"
 )
 
-ft <- flextable(table1)
-ft <- color(ft, part = "footer", color = "#666666")
-ft <- set_caption(ft, caption = as_paragraph(
-                               as_b("Table 1: "), "Studies reporting chiton predators. The scientific names were used according to the currently accepted nomenclature in WoRMS.")
+ft <- flextable(table1) %>%
+  color(part = "footer", color = "#666666") %>%
+  set_caption(caption = as_paragraph(
+    as_b("Table 1: "),
+    "Studies reporting chiton predators. The scientific names were used according to the currently accepted nomenclature in WoRMS.")
+) %>%
+  italic(j = c(2:3)) %>%
+  bold(part = "header", bold = TRUE) %>%
+  bold(j = 1, i = ~ !is.na(Animals), bold = TRUE, part = "body") %>%
+  footnote(i = c(18,23), j = 4,
+           value = as_paragraph("Method not specified by original authors."),
+           ref_symbols = "*",
+           part = "body") %>%
+  align_text_col(align = "center", header = TRUE, footer = FALSE) %>%
+  merge_v(j = 1 ) %>%
+  hline(i = c(5,14,15,25,27,45,91,104), part = "body",
+        border = fp_border(color = "black", style = "dashed", width = 1)
 )
-ft <- italic(ft, j= c(2:3))
-ft <- bold(ft,part = "header", bold = TRUE)
-ft <- bold(ft, j = 1, i = ~ !is.na(Animals), bold = TRUE, part = "body")
-ft <- footnote(ft, i = c(18,23), j = 4,
-               value = as_paragraph("Method not specified by original authors."),
-               ref_symbols = "*",
-               part = "body"
-)
 ft
-ft <- align_text_col(ft, align = "center", header = TRUE, footer = FALSE)
-ft
-ft <- merge_v(ft, j = 1 )
-ft
-ft <- hline(ft, i = c(5,14,15,25,27,45,91,104), part = "body",
-            border = fp_border(color = "black", style = "dashed", width = 1)
-            )
-ft
+
